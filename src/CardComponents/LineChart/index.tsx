8d9a2d18b50e4f24d771f480fe2fe4094310bb71
Mustafa Saifee <mustafa.saifee@undp.org>
2023-02-21 16:18:20 +0200

    Update line chart with only one value

--- src/CardComponents/LineChart/index.tsx
+++ src/CardComponents/LineChart/index.tsx
@@ -35,6 +35,27 @@ const SourceEl = styled.div`
   color: var(--gray-500);
 `;
 
+const StatEl = styled.h3`
+  font-size: 4.375rem;
+  line-height: 0.75;
+  text-shadow: none;
+  -webkit-text-stroke: 2px var(--black);
+  color: var(--gray-200);
+  letter-spacing: .05rem;
+  margin-top: 0;
+  margin-bottom: 1rem;
+`;
+
+const YearEl = styled.span`
+  font-size: 2.5rem;
+  line-height: 1.09;
+  text-shadow: none;
+  -webkit-text-stroke: 0px var(--black);
+  color: var(--gray-500);
+  margin-top: 0;
+  margin-bottom: 1rem;
+`;
+
 export function LineChart(props: Props) {
   const {
     data, lineColor, graphTitle, strokeWidth, suffix, source,
@@ -61,31 +82,50 @@ export function LineChart(props: Props) {
           flexGrow: 1,
           flexDirection: 'column',
           display: 'flex',
+          justifyContent: 'center',
         }}
       >
-        <h2 className='undp-typography bold margin-bottom-00'>
-          {mouseOverData.value}
-          {suffix || ''}
-          {' '}
-          <span style={{ opacity: '0.25' }}>
-            (
-            {mouseOverData.year}
-            )
-          </span>
-        </h2>
-        <div style={{ flexGrow: 1, width: '100%' }} ref={graphDiv}>
-          {svgWidth && svgHeight ? (
-            <LineChartGraph
-              data={data}
-              lineColor={lineColor}
-              svgWidth={svgWidth}
-              svgHeight={svgHeight}
-              strokeWidth={strokeWidth}
-              setMouseOverData={setMouseOverData}
-              mouseOverData={mouseOverData}
-            />
-          ) : null}
-        </div>
+        {
+          data.length > 1 ? (
+            <>
+              <h2 className='undp-typography bold margin-bottom-00'>
+                {mouseOverData.value}
+                {suffix || ''}
+                {' '}
+                <span style={{ opacity: '0.25' }}>
+                  (
+                  {mouseOverData.year}
+                  )
+                </span>
+              </h2>
+              <div style={{ flexGrow: 1, width: '100%' }} ref={graphDiv}>
+                {svgWidth && svgHeight ? (
+                  <>
+                    <LineChartGraph
+                      data={data}
+                      lineColor={lineColor}
+                      svgWidth={svgWidth}
+                      svgHeight={svgHeight}
+                      strokeWidth={strokeWidth}
+                      setMouseOverData={setMouseOverData}
+                      mouseOverData={mouseOverData}
+                    />
+                  </>
+                ) : null}
+              </div>
+            </>
+          ) : (
+            <StatEl>
+              {data[0].value}
+              {' '}
+              <YearEl>
+                (
+                {data[0].year}
+                )
+              </YearEl>
+            </StatEl>
+          )
+        }
       </div>
       <SourceEl className='margin-top-05'>
         Source:
