f79c9cd956c04839ee4ac7d5f3262c3eab8eb92b
Mustafa Saifee <mustafa.saifee@undp.org>
2023-11-09 20:38:07 +0200

    Update OverviewViz.tsx

--- src/OverviewVisualizations/OverviewViz.tsx
+++ src/OverviewVisualizations/OverviewViz.tsx
@@ -12,7 +12,7 @@ interface Props {
 }
 
 const WrapperEl = styled.div`
-  scroll-snap-type: x mandatory;
+  scroll-snap-type: x proximity;
   scroll-padding: 0;
   scroll-padding-left: 0;
   display: flex;
@@ -67,8 +67,8 @@ export function OverviewViz(props: Props) {
           onClick={e => {
             if (WrapperRef.current) {
               if (e.clientX > window.innerWidth / 2)
-                WrapperRef.current.scrollBy(50, 0);
-              else WrapperRef.current.scrollBy(-50, 0);
+                WrapperRef.current.scrollBy(360, 0);
+              else WrapperRef.current.scrollBy(-360, 0);
             }
           }}
           onMouseMove={e => {
