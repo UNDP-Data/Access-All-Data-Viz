e63c03c924b9e72eacb58fe6d0dbcd2cd739ccb8
Mustafa Saifee <mustafa.saifee@undp.org>
2025-12-19 14:55:16 +0200

    Update CheckIfLoginOrNot.tsx

--- src/Utils/CheckIfLoginOrNot.tsx
+++ src/Utils/CheckIfLoginOrNot.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import axios from 'axios';
 
 export const CheckIfLoginOrNot = async () => {
@@ -5,8 +6,10 @@ export const CheckIfLoginOrNot = async () => {
     const response = await axios.get(
       'https://data.undp.org/jsonapi/user/login_status',
     );
+    console.log('Login status response:', response.data);
     return response.data.logged_in;
   } catch (error) {
+    console.log('Error checking login status:', error);
     return false;
   }
 };
